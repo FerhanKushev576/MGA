5ab2e5c6caf73818d961abd0e2baa4aa6348e3f0
Gordon Heiczman <gogo199432@gmail.com>
2021-01-12 17:43:06 +0100

    Force push so gitignore is correct. Updated to 2019.4.17f1 (LTS). Changed player model

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    bool alive = true;
+    private Animator animator;
+    public float speed = 5;
+    [SerializeField] Rigidbody rb;
+
+    float horizontalInput;
+    [SerializeField] float horizontalMultiplier = 2;
+
+    public float speedIncreasePerPoint = 0.1f;
+
+    private void FixedUpdate()
+    {
+        if (!alive) return;
+
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (transform.position.y < -3)
+        {
+            Die();
+        }
+        animator.SetFloat("Speed", rb.velocity.x);
+        animator.SetBool("Alive", alive);
+    }
+
+    public void Die()
+    {
+        alive = false;
+        // Restart the game
+        Invoke("Restart", 2);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
